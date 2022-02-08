cb1cd74ebec85995c9024490c60caa0a3098b58b
DHAPARK <pdh4182@gmail.com>
2022-02-08 22:02:08 +0900

    cache clear

--- blockchainApp/components/LoginComponents/LoginContents.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-
-const Contents = styled.ScrollView`
-    flex : 1;
-    padding: 24px;
-    background-color:white;
-`;
-
-export default Contents;
--- blockchainApp/components/RegistComponents/RegistContainer.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-const Container = styled.SafeAreaView`
-    flex : 1;
-    background-color:white;
-`;
-
-export default Container;
